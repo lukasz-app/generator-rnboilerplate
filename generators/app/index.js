ba91f5de98fb2e549c106de00c596f0e939c0d31
lukasz <lchludzinski@iterato.rs>
2019-05-26 01:09:05 +0200

    lib update, bundle name change

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,8 +2,13 @@ var Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
 const Git = require("./utils/git");
+const replace = require("replace-in-file");
+
+const defIOSBuindleIdentifier = "org.reactjs.native.example";
+const defAndroidPackageName = "com";
+
 module.exports = class extends Generator {
-  prompting() {
+  welcome() {
     this.log(
       yosay(
         `Welcome to the sensational ${chalk.red(
@@ -11,25 +16,35 @@ module.exports = class extends Generator {
         )} generator!`
       )
     );
-
+  }
+  prompting() {
     const prompts = [
       {
         type: "input",
         name: "appName",
         message: "Application name",
-        default: "mobileApplication"
+        default: "mobileApplication",
+        filter: name => name.toLowerCase(),
+        transformer: name => name.toLowerCase()
       },
       {
         type: "input",
         name: "author",
         message: "Author",
         default: "Lukasz Mistrz"
+      },
+      {
+        type: "input",
+        name: "bundleId",
+        message: "Bundle id",
+        default: ({ appName }) => `org.lukaszchopin.${appName}`,
+        filter: name => name.toLowerCase(),
+        transformer: name => name.toLowerCase()
       }
     ];
 
     return this.prompt(prompts).then(props => {
       this.props = props;
-      this.log("app name : ", this.props.appName);
     });
   }
 
@@ -57,6 +72,79 @@ module.exports = class extends Generator {
     this.git.commit("Init commit");
   }
 
+  changeBundleIdIos() {
+    const { appName, bundleId } = this.props;
+    const newBundleId = bundleId.endsWith("." + appName)
+      ? bundleId.replace(new RegExp("." + appName + "$"), "")
+      : bundleId;
+    const iosDefIDRegEx = new RegExp(defIOSBuindleIdentifier, "g");
+    const options = {
+      files: [
+        `ios/${appName}.xcodeproj/project.pbxproj`,
+        `ios/${appName}-tvOS/Info.plist`,
+        `ios/${appName}-tvOSTests/Info.plist`
+      ],
+      from: iosDefIDRegEx,
+      to: newBundleId
+    };
+    try {
+      const changes = replace.sync(options);
+      console.log("Modified files:", changes.join(", "));
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+    this.git.commit("Changed iOS bundleId");
+  }
+
+  changePackageName() {
+    const { appName, bundleId } = this.props;
+    const newBundleId = bundleId.endsWith("." + appName)
+      ? bundleId
+      : bundleId + "." + appName;
+    const androidDefIDRegEx = new RegExp(
+      defAndroidPackageName + "." + appName,
+      "g"
+    );
+    const options = {
+      files: [
+        `android/app/build.gradle`,
+        `android/app/BUCK`,
+        `android/app/src/main/AndroidManifest.xml`,
+        `android/app/src/main/java/${defAndroidPackageName}/${appName}/MainActivity.java`,
+        `android/app/src/main/java/${defAndroidPackageName}/${appName}/MainApplication.java`
+      ],
+      from: androidDefIDRegEx,
+      to: newBundleId
+    };
+    try {
+      const changes = replace.sync(options);
+      console.log("Modified files:", changes.join(", "));
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+    const newDirectoryStructure = newBundleId.split(".");
+    const newDirectoryStructureString = newBundleId.split(".").join("/");
+    for (let i = 0; i < newDirectoryStructure.length; i++) {
+      let path = `android/app/src/main/java`;
+      for (let j = 0; j <= i; j++) {
+        path = path + "/" + newDirectoryStructure[j];
+      }
+      this.spawnCommandSync("mkdir", [path]);
+    }
+    this.spawnCommandSync("mv", [
+      `android/app/src/main/java/${defAndroidPackageName}/${appName}/MainActivity.java`,
+      `android/app/src/main/java/${newDirectoryStructureString}/MainActivity.java`
+    ]);
+    this.spawnCommandSync("mv", [
+      `android/app/src/main/java/${defAndroidPackageName}/${appName}/MainApplication.java`,
+      `android/app/src/main/java/${newDirectoryStructureString}/MainApplication.java`
+    ]);
+    this.spawnCommandSync("rm -rf", [
+      `android/app/src/main/java/${defAndroidPackageName}/${appName}`
+    ]);
+    this.git.commit("Changed android package name");
+  }
+
   writeReadme() {
     this.fs.copyTpl(
       this.templatePath("README.md"),
@@ -65,7 +153,8 @@ module.exports = class extends Generator {
     );
   }
 
-  finishing() {
+  end() {
+    this.git.commit("last commit");
     this.spawnCommandSync("code", ["."]);
     this.log("Happend!!!");
   }
